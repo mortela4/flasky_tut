761ab56aaeed2549cf2e9b5a1181ae8aad490842
mortela4 <morten.larsen@7sense.no>
2019-02-03 20:46:53 +0100

    Step two of 'wtf_ex2' (using subclassing) - via 'step_two()' routing function - now works OK.

--- wtf_ex2.py
+++ wtf_ex2.py
@@ -14,7 +14,7 @@ class RegistrationForm(FlaskForm):
     # Fields:
     username = StringField(label='User name', validators=[validators.Length(min=4, max=25)])
     age = IntegerField(label='Age', validators=[validators.NumberRange(min=18, max=150)])
-    accept_rules = BooleanField(label='I accept the site rules', validators=[validators.InputRequired()])
+    accept_rules = BooleanField(label='I accept the site rules', validators=[])
     sub_mittor = SubmitField(label="Check and register user ...")
     # street_name = None
     # street_number = None
@@ -48,40 +48,42 @@ def step_two():
         # DEBUG:
         print("Got street name: '%s' and street number = %d" % (streetname, streetnum))
         #
-    print("Handling step2 GET ...")
-    # Fill in form:
-    u_name = session.get('u_name')
-    u_age = session.get('u_age')
-    step_one_taken = session.get('u_accept')
-    if not step_one_taken:
-        flash('ERROR: first step NOT taken!')
-        return
+        return redirect("/")
     else:
-        print("First step taken OK! Proceeding with u_name=%s, u_age=%d ..." % (u_name, int(u_age)))
-        reg_form_ext.accept_rules.data = step_one_taken
-        reg_form_ext.username.data = u_name
-        reg_form_ext.age.data = u_age
-        #
-        print("Extending FORM ...")
-        #
-        # Extending w. 'setattr()' doesn't funk ... :-/ (Probable reason: attribute IS already there!!)
-        """
-        setattr(reg_form_ext, 'street_name',
-                StringField(label='Street name', name='street_name', validators=[validators.Length(min=4, max=25)]))
-        setattr(reg_form_ext, 'street_number', IntegerField(label='Street number', name='street_number',
-                                               validators=[validators.NumberRange(min=1, max=999)]))
-        setattr(reg_form_ext, 'add_user', SubmitField(label="Add user!", name='add_user'))
-        #
-        # Using assignment instead doesn't work EITHER!
-        reg_form.street_name = StringField(label='Street name', name='street_name',
-                                           validators=[validators.Length(min=4, max=25)])
-        reg_form.street_number = IntegerField(label='Street number', name='street_number',
-                                              validators=[validators.NumberRange(min=1, max=999)])
-        reg_form.add_user = SubmitField(label="Add user!", name='add_user')
-        """
-        # Last resort - use separate class ... :-/
-        # Rendering:
-        return render_template('wtf_ex2_step2.html', form=reg_form_ext)
+        print("Handling step2 GET ...")
+        # Fill in form:
+        u_name = session.get('u_name')
+        u_age = session.get('u_age')
+        step_one_taken = session.get('u_accept')
+        if not step_one_taken:
+            flash('ERROR: first step NOT taken!')
+            return redirect("/")
+        else:
+            print("First step taken OK! Proceeding with u_name=%s, u_age=%d ..." % (u_name, int(u_age)))
+            reg_form_ext.accept_rules.data = step_one_taken
+            reg_form_ext.username.data = u_name
+            reg_form_ext.age.data = u_age
+            #
+            print("Extending FORM ...")
+            #
+            # Extending w. 'setattr()' doesn't funk ... :-/ (Probable reason: attribute IS already there!!)
+            """
+            setattr(reg_form_ext, 'street_name',
+                    StringField(label='Street name', name='street_name', validators=[validators.Length(min=4, max=25)]))
+            setattr(reg_form_ext, 'street_number', IntegerField(label='Street number', name='street_number',
+                                                   validators=[validators.NumberRange(min=1, max=999)]))
+            setattr(reg_form_ext, 'add_user', SubmitField(label="Add user!", name='add_user'))
+            #
+            # Using assignment instead doesn't work EITHER!
+            reg_form.street_name = StringField(label='Street name', name='street_name',
+                                               validators=[validators.Length(min=4, max=25)])
+            reg_form.street_number = IntegerField(label='Street number', name='street_number',
+                                                  validators=[validators.NumberRange(min=1, max=999)])
+            reg_form.add_user = SubmitField(label="Add user!", name='add_user')
+            """
+            # Last resort - use separate class ... :-/
+            # Rendering:
+            return render_template('wtf_ex2_step2.html', form=reg_form_ext)
 
 
 @app.route('/', methods=["GET", "POST"])
